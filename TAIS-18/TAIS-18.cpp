035c39d96f34961119b6abe22a1314f9b79414b1
Daniel Castro <castrodaniel18@hotmail.com>
2021-10-22 12:11:50 +0200

    uwu

--- TAIS-18/TAIS-18.cpp
+++ TAIS-18/TAIS-18.cpp
@@ -7,31 +7,46 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "Digrafo.h"
 
 using namespace std;
 
 // función que resuelve el problema
 // comentario sobre el coste, O(f(N)), donde N es ...
+class Sumidero{
+public:
+    Sumidero(Digrafo const & grafo): sumidero(-1){
+        sumidero = 0;
+        for(int i = 1; i < grafo.V(); i++){
+            if(grafo.hayArista(sumidero, i))
+                sumidero = i;
+        }
+        for(int i = 0; i < grafo.V() && sumidero != -1; i++){
+            if(i != sumidero && (grafo.hayArista(sumidero, i) || !grafo.hayArista(i, sumidero)))
+                sumidero = -1;
+        }
+    }
 
+    int haySumidero(){ return sumidero; }
+private:
+    int sumidero;
+
+};
 
 // resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
-    int V, A, v, w;
     // leer los datos de la entrada
-    cin >> V;
+    Digrafo grafo(cin);
+
     if (!std::cin)  // fin de la entrada
     return false;
 
-    vector<vector<int>> grafo(V * V + 1);
+    Sumidero sumidero(grafo);
 
-    cin >> A;
-    for(int i = 0; i < A; i++){
-        cin >> v >> w;
-        grafo[v][w] = 1;
-    }
-
-    //Solucion sol = resolver(datos);
+    int sol = sumidero.haySumidero();
+    if(sol != -1) cout << "SI " << sol << "\n";
+    else cout << "NO" << "\n";
 
 // escribir sol
 
